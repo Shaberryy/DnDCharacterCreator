22ef3e1c5606383df3c362ef9c797342ccc1c763
shaberry <jaden.png02@gmail.com>
2023-09-13 09:02:21 -0700

    crypress

--- client/src/components/Characters.jsx
+++ client/src/components/Characters.jsx
@@ -4,6 +4,9 @@ import Builder from "./Builder";
 export default function characterSheets({ characterSheets }){
     return(
         <>
+        <div>
+            <h1>Newly Created Characters</h1>
+        </div>
         {allCharacterSheets.map((characterSheets) => {
             return <Builder key={characterSheets.charactersheetId} characterSheets={characterSheets}/>
         })}
